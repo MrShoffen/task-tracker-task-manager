65167683a27230696f95390c229b2658c6bc1db6
andrey_vasiliev <mrshoffen@gmail.com>
2025-05-31 00:24:42 +0300

    feat: add index

--- src/main/resources/db/changelog/task-table.sql
+++ src/main/resources/db/changelog/task-table.sql
@@ -4,13 +4,17 @@
 --changeset mrshoffen:2
 CREATE TABLE IF NOT EXISTS tasks
 (
-    id             UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name           VARCHAR(256) NOT NULL,
-    created_at     TIMESTAMP    NOT NULL,
-    completed      BOOLEAN      NOT NULL,
-    order_index    BIGINT       NOT NULL,
-    user_id        UUID         NOT NULL,
-    workspace_id   UUID         NOT NULL,
-    desk_id        UUID         NOT NULL,
+    id           UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name         VARCHAR(256) NOT NULL,
+    created_at   TIMESTAMP    NOT NULL,
+    completed    BOOLEAN      NOT NULL,
+    order_index  BIGINT       NOT NULL,
+    user_id      UUID         NOT NULL,
+    workspace_id UUID         NOT NULL,
+    desk_id      UUID         NOT NULL,
     UNIQUE (desk_id, name)
 );
+
+CREATE INDEX IF NOT EXISTS tasks_workspace_id_id_idx ON desks (workspace_id, id);
+
+CREATE INDEX IF NOT EXISTS tasks_workspace_id_idx ON desks (workspace_id);
